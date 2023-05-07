45ee49129bec94b63d123793855ade9d92570c2d
王承毅 <44698391+wangchenyi1996@users.noreply.github.com>
2023-05-07 21:14:12 +0800

    增加mock数据

---
+++ src/config/data.js
@@ -0,0 +1,443 @@
+const swiperList_response = {
+    "swiperList": [
+        {
+            "id": 1,
+            "url": "https://public.danke.com.cn/public-20191015-FojGewWlpLqD4RMxt_7VrXhPzqsN"
+        },
+        {
+            "id": 2,
+            "url": "https://public.danke.com.cn/public-20190908-Fjit4zQaBAN1GTExqrl3r1OwYN-8"
+        },
+        {
+            "id": 3,
+            "url": "https://public.danke.com.cn/public-20190606-Fn47vVrRsxTLdwr3CaUoSZJ6jzzl"
+        },
+        {
+            "id": 4,
+            "url": "https://public.danke.com.cn/public-20191009-FiZYvj4GTuQ3lP7kYIbT6NJ0kwWF"
+        }
+    ],
+    "msg": "ok",
+    "code": 200
+}
+
+
+
+const roomList_response = {
+    "data": [
+        {
+            "roomImg": "http://lc-00shgioj.cn-n1.lcfile.com/b21aa716b308d307b501/1.jpg",
+            "roomWhere": "龙眠大道 天景山公寓花锦苑 主卧 朝南 B室",
+            "type": 1,
+            "price": 680,
+            "oldPrice": 1380,
+            "roomIntro": "四室一厅",
+            "roomArea": "120.8m²",
+            "roomLive": "18/30层",
+            "whereInfo": "距1号线龙眠大道站550米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00shgioj.cn-n1.lcfile.com/d5bb3068effa86302c73/2.jpg",
+            "roomWhere": "集庆门大街 万达东坊 次卧 朝西 A室",
+            "type": 2,
+            "price": 681,
+            "oldPrice": 1381,
+            "roomIntro": "三室一厅",
+            "roomArea": "102.8m²",
+            "roomLive": "10/25层",
+            "whereInfo": "距2号线集庆门大街站500米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00shgioj.cn-n1.lcfile.com/dcad1d3924d7ba47fe28/3.jpg",
+            "roomWhere": "柳洲东路 浦泰和天下三期 朝南 B室",
+            "type": 3,
+            "price": 682,
+            "oldPrice": 1382,
+            "roomIntro": "四室一厅",
+            "roomArea": "135.8m²",
+            "roomLive": "16/30层",
+            "whereInfo": "距3号线柳洲东路站850米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/cb906e925d4574a51a29/4.jpg",
+            "roomWhere": "南京南站  龙西新寓  朝北 D室",
+            "type": 4,
+            "price": 683,
+            "oldPrice": 1383,
+            "roomIntro": "四室一厅",
+            "roomArea": "120.8m²",
+            "roomLive": "18/30层",
+            "whereInfo": "距1号线天印大道站550米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/6c0840d317916c20566f/5.jpg",
+            "roomWhere": "贾西  东升园 主卧 朝南 C室",
+            "type": 5,
+            "price": 684,
+            "oldPrice": 1384,
+            "roomIntro": "四室一厅",
+            "roomArea": "122.8m²",
+            "roomLive": "10/35层",
+            "whereInfo": "距3号线贾西站250米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/b4d227c452cf3ad7d39c/6.jpg",
+            "roomWhere": " 龙江  清江花苑明月园  朝南 B室",
+            "type": 6,
+            "price": 685,
+            "oldPrice": 1385,
+            "roomIntro": "四室一厅",
+            "roomArea": "135.8m²",
+            "roomLive": "10/20层",
+            "whereInfo": "距4号线|9号线龙江站1050米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/46e7468207af0973c578/7.jpg",
+            "roomWhere": "龙眠大道 天景山公寓花锦苑 主卧 朝南 B室",
+            "type": 7,
+            "price": 686,
+            "oldPrice": 1386,
+            "roomIntro": "四室一厅",
+            "roomArea": "120.8m²",
+            "roomLive": "18/30层",
+            "whereInfo": "距1号线龙眠大道站550米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/ad813bc50e5c92bcf234/8.jpg",
+            "roomWhere": "集庆门大街 万达东坊 次卧 朝西 A室",
+            "type": 8,
+            "price": 687,
+            "oldPrice": 1387,
+            "roomIntro": "三室一厅",
+            "roomArea": "102.8m²",
+            "roomLive": "10/25层",
+            "whereInfo": "距2号线集庆门大街站500米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/ed882f7929b6f60846df/9.jpg",
+            "roomWhere": "柳洲东路 浦泰和天下三期 朝南 B室",
+            "type": 9,
+            "price": 688,
+            "oldPrice": 1388,
+            "roomIntro": "四室一厅",
+            "roomArea": "135.8m²",
+            "roomLive": "16/30层",
+            "whereInfo": "距3号线柳洲东路站850米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/d7f31d75b879b0ba6343/10.jpg",
+            "roomWhere": "南京南站  龙西新寓  朝北 D室",
+            "type": 10,
+            "price": 689,
+            "oldPrice": 1389,
+            "roomIntro": "四室一厅",
+            "roomArea": "120.8m²",
+            "roomLive": "18/30层",
+            "whereInfo": "距1号线天印大道站550米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/ed6222c5a9303c06ac2b/11.jpg",
+            "roomWhere": "贾西  东升园 主卧 朝南 C室",
+            "type": 11,
+            "price": 690,
+            "oldPrice": 1390,
+            "roomIntro": "四室一厅",
+            "roomArea": "122.8m²",
+            "roomLive": "10/35层",
+            "whereInfo": "距3号线贾西站250米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/d0e5aa88a19eea22cc25/12.jpg",
+            "roomWhere": " 龙江  清江花苑明月园  朝南 B室",
+            "type": 12,
+            "price": 691,
+            "oldPrice": 1391,
+            "roomIntro": "四室一厅",
+            "roomArea": "135.8m²",
+            "roomLive": "10/20层",
+            "whereInfo": "距4号线|9号线龙江站1050米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/47f8dc7b5b63549cae33/15.jpg",
+            "roomWhere": "南京南站  龙西新寓  朝北 D室",
+            "type": 13,
+            "price": 692,
+            "oldPrice": 1392,
+            "roomIntro": "四室一厅",
+            "roomArea": "120.8m²",
+            "roomLive": "18/30层",
+            "whereInfo": "距1号线天印大道站550米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/9671b016bf88c1de5714/14.jpg",
+            "roomWhere": "贾西  东升园 主卧 朝南 C室",
+            "type": 14,
+            "price": 693,
+            "oldPrice": 1393,
+            "roomIntro": "四室一厅",
+            "roomArea": "122.8m²",
+            "roomLive": "10/35层",
+            "whereInfo": "距3号线贾西站250米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        },
+        {
+            "roomImg": "http://lc-00SHgiOJ.cn-n1.lcfile.com/aa410bbd3920ed16c18c/13.jpg",
+            "roomWhere": " 龙江  清江花苑明月园  朝南 B室",
+            "type": 15,
+            "price": 694,
+            "oldPrice": 1394,
+            "roomIntro": "四室一厅",
+            "roomArea": "135.8m²",
+            "roomLive": "10/20层",
+            "whereInfo": "距4号线|9号线龙江站1050米",
+            "label": [
+                "首月减免",
+                "独立阳台",
+                "地铁周边"
+            ]
+        }
+    ],
+    "msg": "ok",
+    "code": 200
+}
+
+
+const getCharts_response = {
+    "code": 200,
+    "chartData": {
+        "columns": [
+            "日期",
+            "访问用户",
+            "下单用户",
+            "下单率"
+        ],
+        "rows": [
+            {
+                "日期": "1/1",
+                "访问用户": 1393,
+                "下单用户": 1093,
+                "下单率": 0.32
+            },
+            {
+                "日期": "1/2",
+                "访问用户": 1394,
+                "下单用户": 1094,
+                "下单率": 1.32
+            },
+            {
+                "日期": "1/3",
+                "访问用户": 1395,
+                "下单用户": 1095,
+                "下单率": 2.3200000000000003
+            },
+            {
+                "日期": "1/4",
+                "访问用户": 1396,
+                "下单用户": 1096,
+                "下单率": 3.3200000000000003
+            },
+            {
+                "日期": "1/5",
+                "访问用户": 1397,
+                "下单用户": 1097,
+                "下单率": 4.32
+            },
+            {
+                "日期": "1/6",
+                "访问用户": 1398,
+                "下单用户": 1098,
+                "下单率": 5.32
+            }
+        ]
+    }
+}
+
+
+const getTabbar_response = {
+    "tabList": [
+        {
+            "iconPath": "http://cdn.gek6.cn/fr-uni-app-icon/a2.png",
+            "selectedIconPath": "http://cdn.gek6.cn/fr-uni-app-icon/a1.png",
+            "title": "首页",
+            "page": "index",
+            "size": null
+        },
+        {
+            "iconPath": "http://cdn.gek6.cn/fr-uni-app-icon/b2.png",
+            "selectedIconPath": "http://cdn.gek6.cn/fr-uni-app-icon/b1.png",
+            "title": "产品中心",
+            "page": "goods",
+            "size": null
+        },
+        {
+            "iconPath": "http://cdn.gek6.cn/fr-uni-app-icon/c2.png",
+            "selectedIconPath": "http://cdn.gek6.cn/fr-uni-app-icon/c1.png",
+            "title": "发布",
+            "page": "contact",
+            "size": "big"
+        },
+        {
+            "iconPath": "http://cdn.gek6.cn/fr-uni-app-icon/d2.png",
+            "selectedIconPath": "http://cdn.gek6.cn/fr-uni-app-icon/d1.png",
+            "title": "新闻中心",
+            "page": "news",
+            "size": null
+        },
+        {
+            "iconPath": "http://cdn.gek6.cn/fr-uni-app-icon/e2.png",
+            "selectedIconPath": "http://cdn.gek6.cn/fr-uni-app-icon/e1.png",
+            "title": "关于我们",
+            "page": "about",
+            "size": null
+        }
+    ]
+}
+
+const video_respopnse = {
+    "list": [
+        {
+            "video_id": 119,
+            "video_describe": "还有没有更好的防盗技巧？？？  #防偷技能 #防抢  #这波操作稳了@小助手",
+            "cover_url": "http://lc-3resYveE.cn-n1.lcfile.com/0716522695cdc609d6f4/tou5.jpg",
+            "video_url": "http://admin.jzldy.com:8999/utuifiles/video/1.mp4",
+            "user_id": "85141798205",
+            "total_praise": 0,
+            "total_share": 0,
+            "total_discuss": 0,
+            "self_praise": false,
+            "user": {
+                "user_id": 85141798205,
+                "nick_name": "芳舒初?"
+            }
+        },
+        {
+            "video_id": 120,
+            "video_describe": "我是人间小宝贝",
+            "cover_url": "http://lc-3resYveE.cn-n1.lcfile.com/fa07192afab8782a961f/tou8.jpg",
+            "video_url": "http://admin.jzldy.com:8999/utuifiles/video/2.mp4",
+            "user_id": "58478988191",
+            "total_praise": 0,
+            "total_share": 0,
+            "total_discuss": 0,
+            "self_praise": false,
+            "user": {
+                "user_id": 85141798206,
+                "nick_name": "茜茜超甜ovo"
+            }
+        },
+        {
+            "video_id": 121,
+            "video_describe": "什么样的身高差才是最佳情侣身高呢？我174",
+            "cover_url": "http://lc-3resYveE.cn-n1.lcfile.com/b429a95b65f7a911776f/tou3.jpg",
+            "video_url": "http://admin.jzldy.com:8999/utuifiles/video/4.mp4",
+            "user_id": "2229409729479767",
+            "total_praise": 0,
+            "total_share": 0,
+            "total_discuss": 0,
+            "self_praise": false,
+            "user": {
+                "user_id": 85141798207,
+                "nick_name": "小喵～（互）"
+            }
+        },
+        {
+            "video_id": 122,
+            "video_describe": "你是从那个视频认识我的?",
+            "cover_url": "http://lc-3resYveE.cn-n1.lcfile.com/a76bcb203d430e7cda8a/tou1.jpg",
+            "video_url": "http://admin.jzldy.com:8999/utuifiles/video/2.mp4",
+            "user_id": "57436973002",
+            "total_praise": 0,
+            "total_share": 0,
+            "total_discuss": 0,
+            "self_praise": false,
+            "user": {
+                "user_id": 85141798208,
+                "nick_name": "允洛（9月20百日庆✨）"
+            }
+        },
+        {
+            "video_id": 123,
+            "video_describe": "一个女孩突然把头发扎起来，开始玩所有的社交软件对谁都聊得来，眼里的光却没了，你说是为什么？",
+            "cover_url": "http://lc-3resYveE.cn-n1.lcfile.com/77b5a9e6ebeb6cddd527/tou6.jpg",
+            "video_url": "http://admin.jzldy.com:8999/utuifiles/video/ad-wz.mp4",
+            "user_id": "102993998403",
+            "total_praise": 0,
+            "total_share": 0,
+            "total_discuss": 0,
+            "self_praise": false,
+            "user": {
+                "user_id": 85141798209,
+                "nick_name": "你得宠着宝宝呀. ??"
+            }
+        }
+    ]
+}
